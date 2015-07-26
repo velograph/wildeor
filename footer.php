7b7b05606b776cceab8b47ed1de28822f0109358
Jason Britton <velograph@gmail.com>
2015-07-26 14:59:59 -0700

    Beginning set up of header, front page, and footer sections.

--- footer.php
+++ footer.php
@@ -11,7 +11,58 @@
 	</div><!-- #content -->
 
 	<footer class="footer" role="contentinfo">
-		<div class="site-info">
+
+		<!-- Begin Partner Logos -->
+
+		<?php if( have_rows('partner_logos', 27) ) : ?>
+
+			<div class="partner-logos">
+
+			<?php while ( have_rows('partner_logos', 27) ) : ?>
+
+				<?php the_row(); ?>
+
+				<?php if( get_row_layout() == 'partner' ) : ?>
+
+					<div class="partner">
+
+						<a href="<?php the_sub_field('partner_link', 27); ?>">
+							<img src="<?php the_sub_field('logo', 27); ?>" />
+						</a>
+
+					</div>
+
+				<?php endif; ?>
+
+			<?php endwhile; ?>
+
+		</div>
+
+		<?php endif; ?>
+
+		<!-- End Partner Logos -->
+
+		<!-- Begin Footer -->
+
+		<div class="footer-row">
+
+			<div class="footer-section">
+				<img src="<?php the_field('pinecone_logo'); ?>" />
+			</div>
+
+			<div class="footer-section">
+				Mailing List
+			</div>
+
+			<div class="footer-section">
+				<?php echo get_template_part('partials/social', 'media'); ?>
+			</div>
+
+		</div>
+
+		<!-- End Footer -->
+
+		<div class="footer-row site-info">
 			&copy; <?php the_time('Y'); ?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
--- front-page.php
+++ front-page.php
@@ -61,9 +61,9 @@ get_header(); ?>
 								</picture>
 							<?php endif; ?>
 
-							<?php the_sub_field('caption'); ?>
-
-							<?php the_sub_field('page_link'); ?>
+							<a href="<?php the_sub_field('page_link'); ?>">
+								<?php the_sub_field('caption'); ?>
+							</a>
 
 						</div>
 
@@ -76,6 +76,78 @@ get_header(); ?>
 			<?php endif; ?>
 
 		<!-- End Flexible Content -->
+
+		<!-- Begin Shop Portals -->
+
+		<?php if( have_rows('portal_content') ) : ?>
+
+			<div class="shop-portal-row">
+
+		    <?php while ( have_rows('portal_content') ) : ?>
+
+		        <?php the_row(); ?>
+
+		        <?php if( get_row_layout() == 'portal' ) : ?>
+
+					<div class="shop-portal">
+
+						<?php $mobile = wp_get_attachment_image_src(get_sub_field('image'), 'mobile'); ?>
+						<?php $tablet = wp_get_attachment_image_src(get_sub_field('image'), 'tablet'); ?>
+						<?php $desktop = wp_get_attachment_image_src(get_sub_field('image'), 'desktop'); ?>
+						<?php $retina = wp_get_attachment_image_src(get_sub_field('image'), 'retina'); ?>
+
+						<?php if( get_sub_field('image') ) : ?>
+							<picture class="home-featured-image">
+								<!--[if IE 9]><video style="display: none"><![endif]-->
+								<source
+									srcset="<?php echo $mobile[0]; ?>"
+									media="(max-width: 500px)" />
+								<source
+									srcset="<?php echo $tablet[0]; ?>"
+									media="(max-width: 860px)" />
+								<source
+									srcset="<?php echo $desktop[0]; ?>"
+									media="(max-width: 1180px)" />
+								<source
+									srcset="<?php echo $retina[0]; ?>"
+									media="(min-width: 1181px)" />
+								<!--[if IE 9]></video><![endif]-->
+								<img srcset="<?php echo $desktop[0]; ?>">
+							</picture>
+						<?php endif; ?>
+
+						<a href="<?php the_sub_field('page_link'); ?>">
+							<?php the_sub_field('title'); ?>
+						</a>
+
+					</div>
+
+		        <?php endif; ?>
+
+		    <?php endwhile; ?>
+
+		</div>
+
+		<?php endif; ?>
+
+		<!-- End Shop Portals -->
+
+		<!-- Begin Page Lead In -->
+
+		<div class="page-lead-in">
+
+			<div class="pinecone-logo">
+				<img src="<?php the_field('pinecone_logo'); ?>" />
+			</div>
+
+			<div class="page-lead-in-content">
+				<h1><?php the_field('lead_in_title'); ?></h1>
+				<?php the_field('lead_in_content'); ?>
+			</div>
+
+		</div>
+
+		<!-- End Page Lead In -->
 
 		<?php endwhile; // end of the loop. ?>
 
