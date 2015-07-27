d9e13f51f478c4a7901daebbf3bdeb11e93a614d
Jason Britton <velograph@gmail.com>
2015-07-26 18:03:59 -0700

    Include featured image and title for blog posts. Update container name.

--- index.php
+++ index.php
@@ -13,20 +13,40 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
+	<div class="content-container">
 
 		<?php if ( have_posts() ) : ?>
 
-			<?php /* Start the Loop */ ?>
 			<?php while ( have_posts() ) : the_post(); ?>
 
-				<?php
-					/* Include the Post-Format-specific template for the content.
-					 * If you want to override this in a child theme, then include a file
-					 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-					 */
-					get_template_part( 'content', get_post_format() );
-				?>
+				<div class="blog-portal">
+
+					<?php $mobile = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'mobile' ); ?>
+					<?php $tablet = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'tablet' ); ?>
+					<?php $desktop = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'desktop' ); ?>
+					<?php $retina = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'retina' ); ?>
+
+					<picture>
+						<!--[if IE 9]><video style="display: none;"><![endif]-->
+						<source
+							srcset="<?php echo $mobile[0]; ?>"
+							media="(max-width: 500px)" />
+						<source
+							srcset="<?php echo $tablet[0]; ?>"
+							media="(max-width: 860px)" />
+						<source
+							srcset="<?php echo $desktop[0]; ?>"
+							media="(max-width: 1180px)" />
+						<source
+							srcset="<?php echo $retina[0]; ?>"
+							media="(min-width: 1181px)" />
+						<!--[if IE 9]></video><![endif]-->
+						<img srcset="<?php echo $desktop[0]; ?>">
+					</picture>
+
+					<h1><?php the_title(); ?></h1>
+
+				</div>
 
 			<?php endwhile; ?>
 
