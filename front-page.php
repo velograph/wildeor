49502f7eebe2cad9837d7eae0b619cdb74267666
Jason Britton <velograph@gmail.com>
2015-07-28 11:12:22 -0700

    Fix the anchor tag placement.

--- front-page.php
+++ front-page.php
@@ -44,28 +44,31 @@ get_header(); ?>
 					<?php $retina = wp_get_attachment_image_src(get_sub_field('image'), 'retina'); ?>
 
 					<?php if( get_sub_field('image') ) : ?>
-						<picture class="home-featured-image">
-							<!--[if IE 9]><video style="display: none"><![endif]-->
-							<source
-								srcset="<?php echo $mobile[0]; ?>"
-								media="(max-width: 500px)" />
-							<source
-								srcset="<?php echo $tablet[0]; ?>"
-								media="(max-width: 860px)" />
-							<source
-								srcset="<?php echo $desktop[0]; ?>"
-								media="(max-width: 1180px)" />
-							<source
-								srcset="<?php echo $retina[0]; ?>"
-								media="(min-width: 1181px)" />
-							<!--[if IE 9]></video><![endif]-->
-							<img srcset="<?php echo $desktop[0]; ?>">
-						</picture>
+						<a href="<?php the_sub_field('page_link'); ?>">
+							<picture class="home-featured-image">
+								<!--[if IE 9]><video style="display: none"><![endif]-->
+								<source
+									srcset="<?php echo $mobile[0]; ?>"
+									media="(max-width: 500px)" />
+								<source
+									srcset="<?php echo $tablet[0]; ?>"
+									media="(max-width: 860px)" />
+								<source
+									srcset="<?php echo $desktop[0]; ?>"
+									media="(max-width: 1180px)" />
+								<source
+									srcset="<?php echo $retina[0]; ?>"
+									media="(min-width: 1181px)" />
+								<!--[if IE 9]></video><![endif]-->
+								<img srcset="<?php echo $desktop[0]; ?>">
+							</picture>
+						</a>
 					<?php endif; ?>
 
-					<a href="<?php the_sub_field('page_link'); ?>">
+					<div class="slide-caption">
 						<?php the_sub_field('caption'); ?>
-					</a>
+						<a href="<?php the_sub_field('page_link'); ?>">Check it Out</a>
+					</div>
 
 				</div>
 
